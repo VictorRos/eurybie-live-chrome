c71508b8e8c2e2c88eb384de9bc8d6f39548c487
Victor Ros <victor_ros@hotmail.fr>
2019-06-13 00:23:05 +0200

    Create TwitchAuth class to handle generation, validation and revokation of a token provided by the Twitch API
    WIP : Create WebhookApp to handle the subcription to a webhook of Twitch
    API

--- functions/src/TwitchAuth.ts
+++ functions/src/TwitchAuth.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 import * as functions from "firebase-functions";
-import fetch from "node-fetch";
+import * as request from "request-promise";
 
 const BASE_URL = "https://id.twitch.tv";
 const OAUTH2_BASE_URL = `${BASE_URL}/oauth2`;
@@ -16,22 +16,40 @@ class TwitchAuth {
         this.__token = "";
     }
 
-    get token() {
+    get TOKEN() {
         return this.__token;
     }
 
-    set token(_token) {
-        this.__token = _token;
+    /**
+     * Generate a new token from Twitch API
+     * @returns {void}
+     */
+    async generateToken() {
+        try {
+            const data = await request.post({
+                url: `${OAUTH2_BASE_URL}/token?client_id=${config.client_id}&client_secret=${config.client_secret}&grant_type=client_credentials`,
+                json: true
+            });
+            this.__token = data.access_token;
+            console.log("Got token from Twitch API");
+        } catch (_err) {
+            console.error(`Error while generating a token from Twitch API - ${_err.message}`);
+        }
     }
 
+    /**
+     * Revoke the current token from Twitch API
+     * @returns {void}
+     */
     async revokeToken() {
-        // Revoke only if the token exists 
+        // Revoke only if the token exists
         if (this.__token) {
             try {
-                await fetch(
-                    `${OAUTH2_BASE_URL}/revoke?client_id=${config.client_id}&token=${this.__token}`,
-                    {method: "POST"}
-                );
+                await request.post({
+                    url: `${OAUTH2_BASE_URL}/revoke?client_id=${config.client_id}&token=${this.__token}`,
+                    headers: {"Authorization": `OAuth ${this.__token}`},
+                    json: true
+                });
                 console.log("Revoked token from Twitch API");
             } catch (_err) {
                 console.error(`Error while revoking the token from Twitch API - ${_err.message}`);
@@ -39,19 +57,23 @@ class TwitchAuth {
         }
     }
 
-    async generateToken() {
+    /**
+     * Validate the current token from Twitch API
+     * @returns {void}
+     */
+    async validateToken() {
         try {
-            const response = await fetch(
-                `${OAUTH2_BASE_URL}/token?client_id=${config.client_id}&client_secret=${config.client_secret}&grant_type=client_credentials`,
-                {method: "POST"}
-            );
-            if (response.ok) {
-                const json = await response.json();
-                this.__token = json.access_token;
-                console.log("Got token from Twitch API");
-            }
+            await request.get({
+                url: `${OAUTH2_BASE_URL}/validate`,
+                headers: {"Authorization": `OAuth ${this.__token}`},
+                json: true
+            });
+            console.log("Token from Twitch API is validated");
         } catch (_err) {
-            console.error(`Error while generating a token from Twitch API - ${_err.message}`);
+            // 401 Error, we generate a new Token
+            if (_err.statusCode === 401) {
+                await this.generateToken();
+            }
         }
     }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,33 @@
 "use strict";
 
-import helloworld from "./functions/helloworld";
-import time from "./functions/time";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as bodyParser from "body-parser";
 
-exports.helloworld = helloworld;
-exports.time = time;
+import TwitchAuth from "./TwitchAuth";
+import WebhookApp from "./WebhookApp";
+
+admin.initializeApp(functions.config().firebase);
+
+const twitchAuth: TwitchAuth = new TwitchAuth();
+const webhookApp: WebhookApp = new WebhookApp(twitchAuth);
+webhookApp.init();
+
+// (async () => {
+//     await twitchAuth.generateToken();
+//     await twitchAuth.validateToken();
+//     await twitchAuth.revokeToken();
+//     await twitchAuth.validateToken();
+//     await twitchAuth.validateToken();
+// })();
+
+// Main application that embedded our functions to the endpoint "/webhook"
+const main: express.Express = express();
+main.use("/webhook", webhookApp.express);
+// Ensure to handle only JSON requests
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({extended: false}));
+
+// webhook represents all exposed functions
+export const webhook: functions.HttpsFunction = functions.https.onRequest(main);
